dad01083e220a293870b854f61e40004fbdcfd04
Dmitriy Averyanov <dimthomas1989@gmail.com>
2023-03-15 22:08:19 +0300

    Add Domain layer

--- app/src/main/java/com/dimthomas/shoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/dimthomas/shoppinglist/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.dimthomas.shoppinglist
+package com.dimthomas.shoppinglist.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.dimthomas.shoppinglist.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
