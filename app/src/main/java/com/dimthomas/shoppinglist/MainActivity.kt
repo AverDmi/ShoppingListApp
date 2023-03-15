3aaebfa372547772dbf0e32738f582c7ea06e123
Dmitriy Averyanov <dimthomas1989@gmail.com>
2023-03-15 20:26:12 +0300

    Initial commit

---
+++ app/src/main/java/com/dimthomas/shoppinglist/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.dimthomas.shoppinglist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
