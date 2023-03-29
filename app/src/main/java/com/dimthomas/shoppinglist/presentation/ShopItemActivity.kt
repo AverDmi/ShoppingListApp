7f115b00bd41b19d71d0720899eda5b8a30dc952
Dmitriy Averyanov <dimthomas1989@gmail.com>
2023-03-29 20:49:16 +0300

    Add ShopItemActivity

---
+++ app/src/main/java/com/dimthomas/shoppinglist/presentation/ShopItemActivity.kt
@@ -0,0 +1,12 @@
+package com.dimthomas.shoppinglist.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dimthomas.shoppinglist.R
+
+class ShopItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shop_item)
+    }
+}
